70f5dc5bb21bee9d00b9250550ca1206e48f815f
Valerijs Jerjomenko <radiant@inbox.lv>
2021-10-21 19:22:19 +0200

    Добавил классы на предикт

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from tensorflow.keras.models import  Model
+from tensorflow.keras.models import  Model, load_model
 from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization, concatenate
 from tensorflow.keras.callbacks import ModelCheckpoint, LambdaCallback
 from tensorflow.keras.optimizers import Adam
@@ -149,13 +149,6 @@ def my_model():
 
   model = Model((input1, input2), x)
 
-  filepath = "modelcheckpoint/ {epoch:02d}.hdf5"
-  save_callback = ModelCheckpoint(
-    filepath,
-    monitors="mae",
-    save_best_only=True
-  )
-
   def on_epoch_end(epoch, logs):
     mae = logs["val_mae"]
     print(f"На эпохе {epoch} средняя ошибка состовляет {mae}")
@@ -168,3 +161,127 @@ def my_model():
 
 
 model2 = my_model()
+
+model = load_model("_528.hdf5")
+
+
+# Класс MyPredict получает наши вводные данные кроме текста описания и возвращает массив который мы скармливаем нейронке на предокт первым параметром.
+
+class MyPredict:
+
+  def __init__(self, town, profile, tehnology, stage_input):
+    # Здесь в классе вводные данные определенны инпутами для тестирования, в реале будем передавать в инит три аргумента которые будут прилетать от пользователя.
+    self.town = town
+    self.profile = profile
+    self.tehnology = tehnology
+    self.stage_input = stage_input
+
+    self.unique_fr = ['Advertising & Marketing', 'Transport & Logistics', 'LegalTech', 'FoodTech',
+                      'FinTech', 'RetailTech', 'Entertainment', 'CleanTech', 'Business Software',
+                      'PropTech', 'E-commerce', 'HRTech', 'EdTech', 'Healthcare',
+                      'Мedia & Communication', 'Consumer Goods & Services', 'Gaming', 'Travel',
+                      'Telecom', 'SportTech', 'Cybersecurity', 'IndustrialTech', 'SafetyTech',
+                      'FashionTech', 'RetailTech', 'InsuranceTech', 'AgroTech', 'SpaceTech']
+
+    self.tehnology_list = ["3D моделирование", "AR/VR", "BigData", "Аддитивные технологии", "Беспилотники",
+                           "Биометрия", "Биотехнологии", "Блокчейн", "Зеленые технологии", "Интернет вещей",
+                           "Искусственный интеллект и машинное обучение", "Компьютерное зрение", "Нанотехнологии",
+                           "Нейротехнологии", "Новые и портативные источники энергии", "Новые материалы",
+                           "Робототехника", None]
+
+    self.stage_list = ["Идея", "Посевная", "Ранний рост"]
+
+  def town_preprocess(self):
+    if self.town.lower() == "москва":
+      return [1, 0]
+    else:
+      return [0, 1]
+
+  def profili_preprocess(self):
+    arg = self.profile
+    out = str(0) * len(self.unique_fr)
+    out = [int(x) for x in out]
+    for i in range(len(self.unique_fr)):
+      if arg == self.unique_fr[i]:
+        out[self.unique_fr.index(arg)] = 1
+        xTrain = out
+        out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    return xTrain
+
+  def getParametrTehnology(self):
+    arg = self.tehnology
+    out = str(0) * len(self.tehnology_list)
+    out = [int(x) for x in out]
+    for i in range(len(out)):
+      if arg in self.tehnology_list:
+        out[self.tehnology_list.index(arg)] = 1
+        x = out
+        out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      elif type(arg) == str and "," in arg:
+        n = arg.split(",")
+        for j in range(len(n)):
+          n[j] = n[j].strip()
+          out[self.tehnology_list.index(n[j])] = 1
+          x = out
+          out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      else:
+        out[17] = 1
+        x = out
+        out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      return x
+
+  def stage_param(self):
+    st = self.stage_input
+    for i in self.stage_list:
+      if st == i:
+        return [1, 0, 0, 0]
+      elif st == i:
+        return [0, 1, 0, 0]
+      elif st == i:
+        return [0, 0, 1, 0]
+      else:
+        return [0, 0, 0, 1]
+
+  def all_together(self):
+    l = []
+    l.append(self.town_preprocess())
+    l.append(self.profili_preprocess())
+    l.append(self.getParametrTehnology())
+    l.append(self.stage_param())
+    xTr = []
+
+    for i in range(len(l)):
+      for j in range(len(l[i])):
+        xTr.append(l[i][j])
+
+    xTr = np.array(xTr)
+    xTr = np.expand_dims(xTr, axis=0)
+
+    return xTr
+
+
+class PredictText:
+
+  def __init__(self, text):
+    self.text = text
+
+  def input_text_preprocessing(self):
+    ls = self.text.split()
+    ls = [x.replace(".", "").replace("-", "").replace(",","") for x in ls]
+    binded = tokenizer.texts_to_sequences(ls)
+    res = []
+    for i in binded:
+      res += i
+    res_pred = tokenizer.sequences_to_matrix([res])
+    return res_pred
+
+
+a = MyPredict("Лондон", "FoodTech", "Зеленые технологии", "Идея")
+for_proba = a.all_together()
+
+d = PredictText("Самая лучшая технология в мире, на базе нанотехнологий с использованием регресивного микробиологического подхода гинетики и алгоритмов пятого уровня в ускоренном режиме.")
+for_proba2 = d.input_text_preprocessing()
+
+proba = model.predict([for_proba, for_proba2])
+print(f"При данных парамтрах фирмы предположительные инвестиции могут составить - {int(proba)} $.")
+
